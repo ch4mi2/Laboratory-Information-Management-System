cd15834f432469f76b7400a48391365440c70d8f
Navojith <navojith22@gmail.com>
2023-03-11 18:31:01 +0530

    patient account done

--- LIMS/frontend/src/components/HeaderComponent/HeaderComponent.js
+++ LIMS/frontend/src/components/HeaderComponent/HeaderComponent.js
@@ -1,12 +1,12 @@
 const HeaderComponent = ({ profileImgSrc, logoImgSrc, username }) => {
   return (
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="#">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <a className="navbar-brand" href="#">
         <img style={{ width: '150px', marginLeft: '1rem' }} src={logoImgSrc} />
       </a>
 
-      <div class="" id="navbarText">
-        <span class="navbar-text">
+      <div className="" id="navbarText">
+        <span className="navbar-text">
           <img src={profileImgSrc} />
           {username}
         </span>
