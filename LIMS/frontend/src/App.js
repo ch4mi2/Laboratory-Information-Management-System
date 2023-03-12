2787c4eb960078407fe6b52d80c16810e38a9d1c
Navojith <navojith22@gmail.com>
2023-03-12 17:02:21 +0530

    fixed layout

--- LIMS/frontend/src/App.js
+++ LIMS/frontend/src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-
 // pages and components
 import PatientList from './pages/PatientList';
 import PatientProfile from './pages/PatientProfile';
@@ -8,9 +7,9 @@ import EditPatientForm from './components/PatientComponents/EditPatientForm';
 import CreatePatientForm from './components/PatientComponents/CreatePatientForm';
 import HeaderComponent from './components/HeaderComponent/HeaderComponent';
 import PendingAccession from './pages/PendingAccession';
-import Expenseslist from './pages/Expenseslist'
-import Accessed from './pages/Accessed'
-import SideNavBar from './components/SideNavComponent/SideNavBar'
+import Expenseslist from './pages/Expenseslist';
+import Accessed from './pages/Accessed';
+import SideNavBar from './components/SideNavComponent/SideNavBar';
 
 // assets
 import mediLineLogo from './assets/common/mediLineLogo.webp';
@@ -24,22 +23,40 @@ function App() {
         username={''}
       />
       <BrowserRouter>
-      <SideNavBar />
-        <div className="pages">
-          <Routes>
-            <Route path="/patient-list" element={<PatientList />} />
-            <Route path="/create-patient" element={<CreatePatientForm />} />
-            <Route path="/patient-profile/:id" element={<PatientProfile />} />
-            <Route path="/patient-profile/:id/addBill" element={<AddBill />} />
-            <Route
-              path="/patient-profile/:id/edit"
-              element={<EditPatientForm />}
-            />
-            <Route 
-              path="/pendingAccession"
-              element={<PendingAccession />}
-            />
-          </Routes>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-3 col-sm-3 col-md-2 col-lg-2">
+              <SideNavBar />
+            </div>
+            <div className="col-9 col-sm-8 col-md-8 col-lg-8">
+              <div className="pages">
+                <Routes>
+                  <Route path="/patient-list" element={<PatientList />} />
+                  <Route
+                    path="/create-patient"
+                    element={<CreatePatientForm />}
+                  />
+                  <Route
+                    path="/patient-profile/:id"
+                    element={<PatientProfile />}
+                  />
+                  <Route
+                    path="/patient-profile/:id/addBill"
+                    element={<AddBill />}
+                  />
+                  <Route
+                    path="/patient-profile/:id/edit"
+                    element={<EditPatientForm />}
+                  />
+                  <Route
+                    path="/pendingAccession"
+                    element={<PendingAccession />}
+                  />
+                </Routes>
+              </div>
+            </div>
+            <div className="col-0 col-sm-1 col-md-2 col-lg-2"></div>
+          </div>
         </div>
       </BrowserRouter>
     </div>
