3bc042caf9a7fc749da378140dff6f5c20f5d954
it21273230 <gamindudammith@gmail.com>
2023-03-14 20:52:55 +0530

    expenses Delete done

--- LIMS/frontend/src/context/ExpensesContext.js
+++ LIMS/frontend/src/context/ExpensesContext.js
@@ -11,6 +11,10 @@ export const expensesReducer = (state, action) =>{
             return{
                 expenses: [action.payload, ...state.expenses]
             } 
+        case 'DELETE_EXPENSES':
+            return{
+                expenses: state.expenses.filter((e) => e._id !== action.payload._id)
+            }
          default:
             return state     
     }
