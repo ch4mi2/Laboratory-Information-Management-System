f73ddcbe28b3d9689dbe2ecfed2720ec0babeed8
Navojith <navojith22@gmail.com>
2023-03-14 11:25:11 +0530

    removed container

--- LIMS/frontend/src/pages/PendingTestResults.js
+++ LIMS/frontend/src/pages/PendingTestResults.js
@@ -1,31 +1,31 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect } from 'react';
 
 const PendingTestResults = () => {
-    const [testResults, setTestResults] = useState(null)
+  const [testResults, setTestResults] = useState(null);
 
-    useEffect(() => {
-        const fetchTestResults = async () => {
-            const response = await fetch('/api/testResult/pendingTests')
-            const json = await response.json()
+  useEffect(() => {
+    const fetchTestResults = async () => {
+      const response = await fetch('/api/testResult/pendingTests');
+      const json = await response.json();
 
-            if(response.ok){
-            setTestResults(json)
-            }
-        }
-        fetchTestResults()
-    }, [])
+      if (response.ok) {
+        setTestResults(json);
+      }
+    };
+    fetchTestResults();
+  }, []);
 
-    return(
-        <div className="container">
-            <div className="testResults">
-                <h1>hello</h1>
-                {testResults && testResults.map((testResult) => (
-                    <p key={testResult._id}>{testResult.patient?.firstName}</p>
-                    
-                ))}
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="">
+      <div className="testResults">
+        <h1>hello</h1>
+        {testResults &&
+          testResults.map((testResult) => (
+            <p key={testResult._id}>{testResult.patient?.firstName}</p>
+          ))}
+      </div>
+    </div>
+  );
+};
 
 export default PendingTestResults;
