5a3eeb0a55340eaaae5d50968e19442be2a40c8f
Navojith <navojith22@gmail.com>
2023-04-18 15:22:31 +0530

    header mail align

--- LIMS/frontend/src/components/HeaderComponent/HeaderComponent.js
+++ LIMS/frontend/src/components/HeaderComponent/HeaderComponent.js
@@ -4,31 +4,49 @@ const HeaderComponent = ({ logoImgSrc, username }) => {
   return (
     <nav
       className="navbar navbar-expand-lg navbar-light bg-light sticky-top"
-      style={{ zIndex: 25555 , justifyContent:'space-between'}}
+      style={{ zIndex: 25555, justifyContent: 'space-between' }}
     >
       <a className="navbar-brand" href="#">
         <img style={{ width: '150px', marginLeft: '2rem' }} src={logoImgSrc} />
       </a>
 
-      <div 
-        className="" id="navbarText"
-        style={{ display:'flex',marginRight:'2rem'}}
+      <div
+        className=""
+        id="navbarText"
+        style={{ display: 'flex', marginRight: '2rem' }}
       >
         <div className="navbar-text dropdown">
-          <a className="dropdown-toggle d-flex align-items-center" href="#" role="button" id="user-dropdown" data-bs-toggle="dropdown" aria-expanded="false">
+          <a
+            className="dropdown-toggle d-flex align-items-center"
+            href="#"
+            role="button"
+            id="user-dropdown"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
             <FaUserCircle size={24} />
-            
           </a>
           <ul className="dropdown-menu" aria-labelledby="user-dropdown">
-            <li><a className="dropdown-item" href="#">Profile</a></li>
-            <li><hr className="dropdown-divider" /></li>
-            <li><a className="dropdown-item" href="#">Logout</a></li>
+            <li>
+              <a className="dropdown-item" href="#">
+                Profile
+              </a>
+            </li>
+            <li>
+              <hr className="dropdown-divider" />
+            </li>
+            <li>
+              <a className="dropdown-item" href="#">
+                Logout
+              </a>
+            </li>
           </ul>
         </div>
-        <div>
-        <span className="ms-3">{username} temporary@gmail.com </span>
+        <div className="mt-1">
+          <span className="ms-3 align-middle">
+            {username} temporary@gmail.com
+          </span>
         </div>
-        
       </div>
     </nav>
   );
